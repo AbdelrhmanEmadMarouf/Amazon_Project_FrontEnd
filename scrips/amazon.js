d5f376f274bc32735c9319ac25fc805571fe2d40
Abdelrhman Emad <abdelrmanemad999@gmail.com>
2026-01-30 01:09:59 +0200

    here i store the cart products in the local storage

--- scrips/amazon.js
+++ scrips/amazon.js
@@ -12,7 +12,7 @@ const product_stars_img = [
     'rating-50.png',
 ]
 const numberOfProducts = products.length;
-const itemsInCart = new Array(numberOfProducts).fill(0);
+let itemsInCart = new Array(numberOfProducts).fill(0);
 const product_grid = document.querySelector('.product-grid');
 const numOfItemsInCart = document.querySelector('#number_of_items_in_cart');
 const searchBar = document.getElementById('search');
@@ -73,6 +73,8 @@ function addItemToTheCart(itemId,selected_item){
     itemsInCart[itemId]+=quantity;
     numberOfItemsInCart+=quantity;
     numOfItemsInCart.innerHTML = `${String(numberOfItemsInCart)}`;
+    console.log(itemsInCart);
+    storeCartItemsIntoLocalStorage();
     
 }
 
@@ -167,10 +169,66 @@ function setProductsInHtml(products_list){
     }
 }
 
+function storeCartItemsIntoLocalStorage(){
+
+    //* transfer Array into Object
+    const obj = itemsInCart.reduce((myObj, currentItemInArray, index) => {
+    myObj[index] = currentItemInArray;
+    return myObj;
+    }, {});
+
+    //*transfer Object into Json
+    const objectInJson = JSON.stringify(obj);
+
+
+    localStorage.setItem('products_in_cart',objectInJson);
+}
+
+
+function getProductCartsFromLocalStorage(){
+
+    //* get Json from local storage
+    const objectInJson = localStorage.getItem('products_in_cart');
+    
+    if(objectInJson === null) return itemsInCart ;
+
+    //* transfer json into object
+    const obj = JSON.parse(objectInJson);
+
+    //* transfer object into array
+    const arr = Object.values(obj);
+
+    return arr;
+    
+}
+
+function getNumberOfItemsInCart(arr){
+    let cnt = 0;
+    for(index in arr){
+        if(arr[index] !== 0){
+            cnt+=arr[index];
+        }
+    }
+
+    return cnt;
+}
+
+function initialization(){
+    setProductsInHtml(products);
+    itemsInCart = getProductCartsFromLocalStorage();
+    numberOfItemsInCart = getNumberOfItemsInCart(itemsInCart);
+    numOfItemsInCart.innerHTML = `${String(numberOfItemsInCart)}`;
+
+}
+
+
+
 
 searchBar.addEventListener('input', function(){
     search(searchBar.value);
 });
 
 
-setProductsInHtml(products);
+getProductCartsFromLocalStorage();
+
+initialization();
